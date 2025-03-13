a1517b000baef0bae4a8633ee46548164d868150
amauget <145220965+amauget@users.noreply.github.com>
2025-03-13 16:23:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -18,16 +18,21 @@ const app = express()
 const path = require('node:path')
 const multer = require('multer')
 
-
+console.log('router')
 const router = require('./routes/router')
 
+console.log('assetPath')
 const assetPath = path.join(__dirname, 'public') //public assets such as stylesheets
+
+console.log('app.use(express.static(assetPath))')
 
 app.use(express.static(assetPath))
 
+console.log('app.use(express.urlencoded({ extended: true }))')
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
+console.log('app.use('/', router)')
 app.use('/', router)
 
 app.use((err, req, res, next) => {
