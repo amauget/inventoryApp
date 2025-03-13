db9f1ea34a826993c010be45ec665233388c667c
amauget <145220965+amauget@users.noreply.github.com>
2025-03-13 16:49:29 -0700

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,19 +1,19 @@
 const { Router } = require('express')
 const router = Router()
-const multer = require('multer')
+// const multer = require('multer')
 
-const {sortFilters, postCar, renderUpload, handleLogin, handleDeletePost, handleSingleRequest} = require('../controllers/controller')
+// const {sortFilters, postCar, renderUpload, handleLogin, handleDeletePost, handleSingleRequest} = require('../controllers/controller')
 
-const categoryList = require('../db/seedDB/categoryList.json')
-const storage = multer.memoryStorage() //prevents upload to server files until after the file is scrubbed/evaluated.
-const upload = multer({storage: storage})
+// const categoryList = require('../db/seedDB/categoryList.json')
+// const storage = multer.memoryStorage() //prevents upload to server files until after the file is scrubbed/evaluated.
+// const upload = multer({storage: storage})
 
 router.get('/test', (req,res) =>{
     console.log('request received!')
 })
 
-router.get('/', async (req, res) => {
-    console.log('TESTING DEPLOYMENT REQUEST')
+// router.get('/', async (req, res) => {
+//     console.log('TESTING DEPLOYMENT REQUEST')
     // let filters = req.query //object with different filters (ie. category, year, make, etc)
     // if(filters.category === undefined){
     //     filters = {category: '*', make: '*'}
@@ -25,104 +25,104 @@ router.get('/', async (req, res) => {
 
     
 
-})
+// })
 
-router.get('/addCar', async (req, res) => {
-    try{
-        const results = await renderUpload(req, res)
+// router.get('/addCar', async (req, res) => {
+//     try{
+//         const results = await renderUpload(req, res)
 
-        res.render('addCar', { results })
+//         res.render('addCar', { results })
     
-    }
-    catch(error){
-        res.render('404')
-    }
-})
+//     }
+//     catch(error){
+//         res.render('404')
+//     }
+// })
 
-router.post('/addCar', upload.any(), async (req, res) => { 
-    //PRICE INPUT HAS A BUG
-    if(!req.files || req.files.length === 0){
-        return res.status(400).json({error: 'No files uploaded'})
-    }
-    if(req.files.length > 10){
-        return res.status(400).json({error:"Limit of 10 images allowed. Quit changing my javascript!"})
-    }
-    if(parseInt(req.body.price) > 1000000 ){
-        res.redirect('/addcar')
-    }
+// router.post('/addCar', upload.any(), async (req, res) => { 
+//     //PRICE INPUT HAS A BUG
+//     if(!req.files || req.files.length === 0){
+//         return res.status(400).json({error: 'No files uploaded'})
+//     }
+//     if(req.files.length > 10){
+//         return res.status(400).json({error:"Limit of 10 images allowed. Quit changing my javascript!"})
+//     }
+//     if(parseInt(req.body.price) > 1000000 ){
+//         res.redirect('/addcar')
+//     }
     
 
-    const fileData = await postCar(req, res, upload)
+//     const fileData = await postCar(req, res, upload)
 
-    //wipe storage image value to prevent duplicate posts.
-    const results = await sortFilters(req, res)
-    res.redirect('/')
+//     //wipe storage image value to prevent duplicate posts.
+//     const results = await sortFilters(req, res)
+//     res.redirect('/')
 
-})
-router.get('/login', (req, res) => {
-    res.render('login', {incorrect: false})
+// })
+// router.get('/login', (req, res) => {
+//     res.render('login', {incorrect: false})
 
-})
-router.get('/loginAttempt', async (req, res) => {
-    const isAdmin = await handleLogin(req, res)
+// })
+// router.get('/loginAttempt', async (req, res) => {
+//     const isAdmin = await handleLogin(req, res)
 
-    if(isAdmin){
-        res.status(200).redirect('deleteAds')
-    }
-    else{
+//     if(isAdmin){
+//         res.status(200).redirect('deleteAds')
+//     }
+//     else{
         
-        res.render('login', {incorrect: true})
-    }
+//         res.render('login', {incorrect: true})
+//     }
 
-})
+// })
 
-router.get('/deleteAds', async (req, res) => {
-    let filters = req.query //object with different filters (ie. category, year, make, etc)
-    if(filters.category === undefined){
-        filters = {category: '*', make: '*'}
-    }
-    const results = await sortFilters(filters)
+// router.get('/deleteAds', async (req, res) => {
+//     let filters = req.query //object with different filters (ie. category, year, make, etc)
+//     if(filters.category === undefined){
+//         filters = {category: '*', make: '*'}
+//     }
+//     const results = await sortFilters(filters)
     
-    res.render('deleteAds', { results: results, data: JSON.stringify(results), loginFailed: false, categoryList: JSON.stringify(categoryList) })
-})
+//     res.render('deleteAds', { results: results, data: JSON.stringify(results), loginFailed: false, categoryList: JSON.stringify(categoryList) })
+// })
 
-router.post('/deleteAds', async (req, res) => {
-    const isAdmin = await handleLogin(req, res)
-    if(isAdmin){
-        const deleted = await handleDeletePost(req, res)
-        let status = 201
-        if(deleted !== true){
-            status = 404
-        }
-        res.redirect('deleteAds')
-    }
-    else{
-        let filters = req.query //object with different filters (ie. category, year, make, etc)
-        if(filters.category === undefined){
-            filters = {category: '*', make: '*'}
-        }
-        const results = await sortFilters(filters)
-        //NEEDED: attach previous ID number to form
-        res.render('deleteAds', { results: results, data: JSON.stringify(results), loginFailed: true, loginFailedID: req.body.postID, categoryList: JSON.stringify(categoryList) })
+// router.post('/deleteAds', async (req, res) => {
+//     const isAdmin = await handleLogin(req, res)
+//     if(isAdmin){
+//         const deleted = await handleDeletePost(req, res)
+//         let status = 201
+//         if(deleted !== true){
+//             status = 404
+//         }
+//         res.redirect('deleteAds')
+//     }
+//     else{
+//         let filters = req.query //object with different filters (ie. category, year, make, etc)
+//         if(filters.category === undefined){
+//             filters = {category: '*', make: '*'}
+//         }
+//         const results = await sortFilters(filters)
+//         //NEEDED: attach previous ID number to form
+//         res.render('deleteAds', { results: results, data: JSON.stringify(results), loginFailed: true, loginFailedID: req.body.postID, categoryList: JSON.stringify(categoryList) })
 
-    }
-})
-router.post('/viewSingle', async (req, res) => {
-    //images are too large to pass through a form as base64
-    const itemImages = await handleSingleRequest(req.body.id)
-    const item = {
-        id: req.body.id,
-        category: req.body.category,
-        year: req.body.year,
-        make: req.body.make,
-        model: req.body.model,
-        trans: req.body.trans,
-        price: req.body.price,
-        description: req.body.description,
-        imgpath: itemImages
-    }
+//     }
+// })
+// router.post('/viewSingle', async (req, res) => {
+//     //images are too large to pass through a form as base64
+//     const itemImages = await handleSingleRequest(req.body.id)
+//     const item = {
+//         id: req.body.id,
+//         category: req.body.category,
+//         year: req.body.year,
+//         make: req.body.make,
+//         model: req.body.model,
+//         trans: req.body.trans,
+//         price: req.body.price,
+//         description: req.body.description,
+//         imgpath: itemImages
+//     }
 
-    res.render('viewSingle', {item})
-});
+//     res.render('viewSingle', {item})
+// });
 
 module.exports = router
